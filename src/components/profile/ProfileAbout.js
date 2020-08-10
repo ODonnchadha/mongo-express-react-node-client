e8eaf5c9856dac9b9a39d8ad90f2f66c38113a56
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-10 14:34:02 -0500

    Initial profile about html and code.

--- src/components/profile/ProfileAbout.js
+++ src/components/profile/ProfileAbout.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isEmpty from '../../validation/isEmpty';
 
 class ProfileAbout extends Component {
   render() {
-    return (
-      <div>
-          ProfileAbout
+    const { profile } = this.props;
+    const firstName = profile.user.name.trim().split(' ')[0];
+
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} class="p-3">
+        <i class="fa fa-check"></i>
+        {skill}
       </div>
+    ));
+
+    return (
+      <div class="row">
+      <div class="col-md-12">
+        <div class="card card-body bg-light mb-3">
+          <h3 class="text-center text-info">{firstName}'s Bio</h3>
+          <p class="lead">
+            {isEmpty(profile.bio) ? null : (<span>{profile.bio}</span>)}
+          </p>
+          <hr />
+          <h3 class="text-center text-info">Skill Set</h3>
+          <div class="row">
+            <div class="d-flex flex-wrap justify-content-center align-items-center">
+              {skills}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -25,32 +25,32 @@ class ProfileHeader extends Component {
             
             <p>
               {isEmpty(profile.website) ? null : (
-                <a className="text-white p-2" href={profile.website} terget="_blank">
+                <a className="text-white p-2" href={profile.website} target="_blank">
                   <i className="fas fa-globe fa-2x"></i>
                 </a>
               )}
               {isEmpty(profile.social && profile.social.twitter) ? null : (
-                <a className="text-white p-2" href={profile.social.twitter} terget="_blank">
+                <a className="text-white p-2" href={profile.social.twitter} target="_blank">
                   <i className="fab fa-twitter fa-2x"></i>
                 </a>
               )}
               {isEmpty(profile.social && profile.social.facebook) ? null : (
-                <a className="text-white p-2" href={profile.social.facebook} terget="_blank">
+                <a className="text-white p-2" href={profile.social.facebook} target="_blank">
                   <i className="fab fa-facebook fa-2x"></i>
                 </a>
               )}
               {isEmpty(profile.social && profile.social.linkedin) ? null : (
-                <a className="text-white p-2" href={profile.social.linkedin} terget="_blank">
+                <a className="text-white p-2" href={profile.social.linkedin} target="_blank">
                   <i className="fab fa-linkedin fa-2x"></i>
                 </a>
               )}
               {isEmpty(profile.social && profile.social.youtube) ? null : (
-                <a className="text-white p-2" href={profile.social.youtube} terget="_blank">
+                <a className="text-white p-2" href={profile.social.youtube} target="_blank">
                   <i className="fab fa-youtube fa-2x"></i>
                 </a>
               )}
               {isEmpty(profile.social && profile.social.instagram) ? null : (
-                <a className="text-white p-2" href={profile.social.instagram} terget="_blank">
+                <a className="text-white p-2" href={profile.social.instagram} target="_blank">
                   <i className="fab fa-instagram fa-2x"></i>
                 </a>
               )}
