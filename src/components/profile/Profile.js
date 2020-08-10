b6fcdecdbe4a576d373ce834481cf32165856f82
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-10 09:28:26 -0500

    Profile header html and associated helper methods.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -34,7 +34,7 @@ class Profile extends Component {
           </div>
           <div className="col-md-6">
           </div>
-          <ProfileHeader />
+          <ProfileHeader profile={profile} />
           <ProfileAbout />
           <ProfileCreds />
           <ProfileGithub />
