5b36cf35f03422f3ee8ea3b80d13738a0f323e91
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-16 08:37:50 -0500

    PostItem introduction.

---
+++ src/components/posts/PostItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostItem extends Component {
+  render() {
+
+    return (
+      <div>
+        PostItem
+      </div>
+    )
+  }
+}
+
+export default PostItem;
