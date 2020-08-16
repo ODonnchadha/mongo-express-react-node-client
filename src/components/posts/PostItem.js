510f3c12267e0a9816f7b6f9fe56d76ef89efdaf
ODonnchadha <danieldonahue1231@hotmail.com>
2020-08-16 09:46:46 -0500

    Additional PostItem binding, etc...

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -5,6 +5,11 @@ import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 
 class PostItem extends Component {
+
+  onDelete(id) {
+    console.log(id);
+  }
+
   render() {
     const { auth, post } = this.props;
 
@@ -13,29 +18,39 @@ class PostItem extends Component {
           <div className="row">
             <div className="col-md-2">
               <a href="profile.html">
-                <img className="rounded-circle d-none d-md-block" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+                <img 
+                  className="rounded-circle d-none d-md-block" 
+                  src={post.avatar}
                   alt="" />
               </a>
               <br />
-              <p className="text-center">John Doe</p>
+              <p className="text-center">
+                {post.name}
+              </p>
             </div>
             <div className="col-md-10">
-              <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus corporis sunt necessitatibus! Minus
-                nesciunt soluta suscipit nobis. Amet accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
-                eveniet cum cupiditate aliquam?</p>
+              <p className="lead">
+                {post.text}
+              </p>
               <button type="button" className="btn btn-light mr-1">
                 <i className="text-info fas fa-thumbs-up"></i>
-                <span className="badge badge-light">4</span>
+                <span className="badge badge-light">
+                  {post.likes.length}
+                </span>
               </button>
               <button type="button" className="btn btn-light mr-1">
                 <i className="text-secondary fas fa-thumbs-down"></i>
               </button>
-              <a href="post.html" className="btn btn-info mr-1">
+              <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
                 Comments
-              </a>
-              <button type="button" className="btn btn-danger mr-1">
+              </Link>
+              {post.user === auth.user.id ? 
+              (<button 
+                onClick={this.onDelete.bind(this, post._id)} 
+                type="button" 
+                className="btn btn-danger mr-1">
                 <i className="fas fa-times" />
-              </button>
+              </button>) : null }
             </div>
           </div>
         </div>
